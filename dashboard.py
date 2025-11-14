e543f9e60328e694fc756612fc91f81aaf902e02
Márk Vince Varga <mark.vince.varga@gmail.com>
2025-11-14 14:13:27 +0100

    add hindcast to graph

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import json
 
 import streamlit as st
+import datetime
 import plotly.graph_objects as go
 import pandas as pd
 import hops
@@ -14,38 +15,63 @@ def load_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     """Load air quality records and predictions from Hopsworks"""
     project = hops.Project(name="ostergotland_air_quality")
     air_quality_fg, forecast_fg = project.get_feature_groups(
-        [("air_quality", 2), ("air_quality_forecasts", 1)]
+        [("air_quality", 2), ("air_quality_forecasts", 3)]
+    )
+    # Only load historical data for the last 14 days
+    last_14_days_filter = air_quality_fg.date >= (
+        datetime.date.today() - datetime.timedelta(days=14)
+    ).strftime("%Y-%m-%d")
+
+    aq_df = air_quality_fg.filter(last_14_days_filter).read().sort_values(by="date")
+    forecast_df = forecast_fg.read().sort_values(by="date")
+    return (
+        aq_df,
+        forecast_df,
     )
 
-    return air_quality_fg.read().sort_values(by="date"), forecast_fg.read().sort_values(
-        by="date"
-    )
+
+def add_forecast_diff(df_forecast: pd.DataFrame) -> pd.DataFrame:
+    """Add column "forecast_days_before" to indicate
+    how many days before the reading the forecast was made"""
+    df_forecast["forecast_days_before"] = (
+        df_forecast["forecast_on"] - df_forecast["date"]
+    ).map(lambda x: abs(x.days))
+    df_forecast["forecast_days_before"] = df_forecast["forecast_days_before"]
+    return df_forecast
 
 
-def create_plot(df_air_quality: pd.DataFrame, df_forecast: pd.DataFrame):
+def create_plot(
+    df_air_quality: pd.DataFrame,
+    df_forecast: pd.DataFrame,
+    forecast_days: tuple[int] = None,
+) -> None:
     """Create and display air quality visualization in Streamlit"""
+    if forecast_days is None:
+        forecast_days = (1, 4, 9)
     fig = go.Figure()
-
     # Add historical data
     fig.add_trace(
         go.Scatter(
             x=df_air_quality["date"],
             y=df_air_quality["pm25"],
             mode="lines",
-            name="Historical PM2.5",
+            name="PM2.5",
         )
     )
-
     # Add forecast data
-    fig.add_trace(
-        go.Scatter(
-            x=df_forecast["date"],
-            y=df_forecast["predicted_pm25"],
-            mode="lines",
-            name="Forecast PM2.5",
-            line=dict(dash="dash"),
+    for days in forecast_days:
+        forecast_days_in_advance = df_forecast[
+            df_forecast["forecast_days_before"] == days
+        ]
+        fig.add_trace(
+            go.Scatter(
+                x=forecast_days_in_advance["date"],
+                y=forecast_days_in_advance["predicted_pm25"],
+                mode="lines",
+                name=f"PM2.5 forecast ({days} days before)",
+                line=dict(dash="dash"),
+            )
         )
-    )
 
     fig.update_layout(
         title="Air Quality: Historical and Forecast",
@@ -65,7 +91,8 @@ st.title("Ostergotland Air Quality Dashboard")
 selected_place = "A77446"
 data_load_state = st.text("Loading data...")
 historical_data, forecast_data = load_data()
-data_load_state.text("Done! (using st.cache_data)")
+forecast_data = add_forecast_diff(forecast_data)
+data_load_state.text("")
 
 hist_for_place, forecast_for_place = (
     historical_data[historical_data["id"] == selected_place],
