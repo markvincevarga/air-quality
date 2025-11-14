3af41be4f81f3cdcf97c9e2b16ec27d222f57482
Márk Vince Varga <mark.vince.varga@gmail.com>
2025-11-14 13:56:39 +0100

    fix historical data only uploading 1 forecast per sensor per day

--- batch_inference_pipeline.py
+++ batch_inference_pipeline.py
@@ -70,16 +70,21 @@ batch_data.rename(columns={"lagged_aq_id": "id"}, inplace=True)
 # Since we don't actually have that data, the join just adds the latest available
 for lag_col in batch_data.columns:
     if lag_col.startswith("lagged_aq_"):
-        batch_data.loc[batch_data["date"] > (datetime.date.today() + datetime.timedelta(days=1)), lag_col] = None
+        batch_data.loc[
+            batch_data["date"] > (datetime.date.today() + datetime.timedelta(days=1)),
+            lag_col,
+        ] = None
 batch_data["predicted_pm25"] = None
-batch_data["predicted_pm25"] = batch_data["predicted_pm25"].astype('float32')
+batch_data["predicted_pm25"] = batch_data["predicted_pm25"].astype("float32")
 batch_data.info()
 # %%
-for day in batch_data["date"].unique():  
+for day in batch_data["date"].unique():
     print("Processing day:", day)
     mask_day = batch_data["date"] == day
     predicted = retrieved_xgboost_model.predict(
-        batch_data[mask_day].drop(columns=["date", "id", "predicted_pm25"]).rename(
+        batch_data[mask_day]
+        .drop(columns=["date", "id", "predicted_pm25"])
+        .rename(
             columns={
                 col: "weather_" + col
                 for col in batch_data[mask_day].columns
@@ -88,26 +93,34 @@ for day in batch_data["date"].unique():
         )
     )
     predicted = predicted.astype(float)
-    batch_data.loc[mask_day,"predicted_pm25"] = predicted
+    batch_data.loc[mask_day, "predicted_pm25"] = predicted
     # Take the weather forecast for tomorrow
     if day == batch_data["date"].max():
         continue
     next_day = day + datetime.timedelta(days=1)
     mask_next = batch_data["date"] == next_day
     # pm25 lagged_1d for tomorrow = predicted_pm25 for today
-    batch_data.loc[mask_next, "lagged_aq_pm25_lagged_1d"] = batch_data.loc[mask_day, "predicted_pm25"].values
-    batch_data.loc[mask_next, "lagged_aq_pm25_lagged_2d"] = batch_data.loc[mask_day, "lagged_aq_pm25_lagged_1d"].values
-    batch_data.loc[mask_next, "lagged_aq_pm25_lagged_3d"] = batch_data.loc[mask_day, "lagged_aq_pm25_lagged_2d"].values
+    batch_data.loc[mask_next, "lagged_aq_pm25_lagged_1d"] = batch_data.loc[
+        mask_day, "predicted_pm25"
+    ].values
+    batch_data.loc[mask_next, "lagged_aq_pm25_lagged_2d"] = batch_data.loc[
+        mask_day, "lagged_aq_pm25_lagged_1d"
+    ].values
+    batch_data.loc[mask_next, "lagged_aq_pm25_lagged_3d"] = batch_data.loc[
+        mask_day, "lagged_aq_pm25_lagged_2d"
+    ].values
 # %%
 # Save forecasts to separate feature group
 batch_data = batch_data[["date", "id", "predicted_pm25"]]
 batch_data["forecast_on"] = datetime.date.today()
+batch_data.head()
 
+# %%
 forecasts_fg = fs.get_or_create_feature_group(
     name="air_quality_forecasts",
     description="Forecasted Air Quality for performance measurements",
-    version=1,
-    primary_key=["id"],
+    version=3,
+    primary_key=["id", "date"],
     event_time="forecast_on",
 )
 forecasts_fg.insert(batch_data)
